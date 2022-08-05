10e5d4a870a558d40984c4db2eaa73902c2dee4f
Juddee <osejuddee@gmail.com>
2022-08-05 06:17:45 -0700

    create dashboard.html & chart-config.js ,updated main.js & main.css

---
+++ portfolia/static/js/main.js
@@ -0,0 +1,10 @@
+function dropdownMenu(menu)
+{
+    var menu = document.getElementById(menu);
+    menu.classList.toggle('dropdown-show');
+}
+function handleMenu()
+{
+    var navMenu = document.getElementById('wrapper');
+    navMenu.classList.toggle('open');
+}
