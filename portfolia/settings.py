83997917f6f7d84e4735c182fec879f6e32548a7
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2022-08-19 11:39:25 +0100

    Automatic configuration with django-heroku

--- portfolia/settings.py
+++ portfolia/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from decouple import config
 import django_heroku
 
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
